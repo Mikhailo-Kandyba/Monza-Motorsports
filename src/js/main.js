ddbd22e14f5e0f95ced9adfadaba04e2106c601e
Mikhailo <mikhailo.kandyba0@gmail.com>
2022-05-06 17:03:19 +0300

    update header and bags fixing

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
+//show-filter
 $(".btn-filter").on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
     $(".filter-area").toggleClass('show-filter');
+});
+
+//btn-menu
+$(".btn-menu").on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    $(".menu-area").toggleClass('show-menu');
+    $("body").toggleClass('overflow-hidden');
 });
